921200054531f177e84a5a065fd93c4e1efa6296
Absintu <alphonsecapone304@gmail.com>
2023-03-19 22:54:26 +0000

    Adding voter functionality to the frontend

---
+++ client/src/AddVoter.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+
+function AddVoter({addVoterCallback}) {
+    const [address, setAddress] = useState("")
+
+    return (
+      <div className="container">
+        <table className="table table-striped table-dark">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">List of voters</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>0x5D448002B2160aa0A7f59B6292aAC11424F7aDA8</td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>0x5D448002B2160aa0A7f59B6292aAC11424F7aDA8</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td>0x5D448002B2160aa0A7f59B6292aAC11424F7aDA8</td>
+                </tr>
+            </tbody>
+        </table>
+        <div className="row row-dark">
+            <div className="col"></div>
+                <input value ={address} onChange={(e) => setAddress(e.target.value)}/>
+                <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={()=>addVoterCallback(address)}
+                >
+                    Add Voter
+                </button>
+            </div>
+            <div className="col">
+                
+            </div>
+        </div>
+    );
+  }
+  
+  export default AddVoter;
--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,40 @@
-import './App.css';
+import './App.css'
 import web3 from './web3'
+import AddVoter from './AddVoter'
+import votingContract from './contracts/Voting'
 
 function App() {
+  /*
+  const contractABI = 'contracts/Voting.json'.abi
+  const contractAddress = 'contracts/Voting.json'.abi.networks.11155111.address
+  console.log(contractABI)
+  console.log(contractAddress)
+  */
+  const contractAddress = votingContract.networks[11155111].address
+  let votingInstance = new web3.eth.Contract(votingContract.abi, contractAddress);
+
+  const addVoter = async (address) => {
+    try{
+      await votingInstance.methods.addVoter(address).send({from: web3.currentProvider.selectedAddress})
+    }
+    catch(e){
+      console.log(e)
+    }
+  }
+  
   return (
     <div className="container">
       <div className="row">
-        CLIENT FRONTEND
+        Client frontend
       </div>
-      <div className="row align-bottom">
-        <h1>Owner of contract Frontend</h1>
-      </div>
-      <div className="row align-bottom">
-        <input />
-      </div>
-      <div className="row align-bottom">
-        <button>Add Voter</button>
+      <div className="row text-center">
+        <h1>Frontend for owner of contract</h1>
+          <div className="col">
+            <AddVoter addVoterCallback={addVoter}/>
+          </div>
+          <div className="col">
+            
+          </div>
       </div>
     </div>
   );
